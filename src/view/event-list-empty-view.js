0a2df4d6910714126f3b2519295c8930794913da
a.nemova <carrie@mail.ru>
2022-12-25 16:37:24 +0300

    Добавляет наследование простым компонентам

--- src/view/event-list-empty-view.js
+++ src/view/event-list-empty-view.js
@@ -1,25 +1,12 @@
-import { createElement } from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 import { Message } from '../const.js';
 
 function createEmptyMessageTemplate() {
   return `<p class="trip-events__msg">${Message.EVERYTHING}</p>`;
 }
 
-export default class EventListEmptyView {
-  #element = null;
+export default class EventListEmptyView extends AbstractView {
   get template() {
     return createEmptyMessageTemplate();
-  }
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
   }
 }
