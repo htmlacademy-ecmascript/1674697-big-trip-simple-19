9960514e2739a9e9ac4f2a756dc01acf4f54f137
a.nemova <carrie@mail.ru>
2023-01-03 17:05:49 +0300

    Фикс ошибок в common.js

--- src/utils/common.js
+++ src/utils/common.js
@@ -3,19 +3,23 @@ import dayjs from 'dayjs';
 function humanizeEventDueDate(dueDate, dateFormat) {
   return dueDate ? dayjs(dueDate).format(dateFormat) : '';
 }
+
 function isStartDateExpired(dateFrom) {
-  return dayjs(dateFrom).isAfter(dayjs())
+  return dayjs(dateFrom).isAfter(dayjs());
 }
+
 function isEndDateExpired(dateTo) {
-  return dayjs(dateTo).isAfter(dayjs())
+  return dayjs(dateTo).isAfter(dayjs());
 }
+
 function isFuturePoint(dateFrom, dateTo) {
-  return isStartDateExpired(dateFrom) && isEndDateExpired(dateTo)
+  return isStartDateExpired(dateFrom) && isEndDateExpired(dateTo);
 }
 
 function isEscapeKey(evt) {
   return evt.key === 'Escape' || evt.key === 'Esc';
 }
+
 function updateItem(items, update) {
   return items.map((item) => item.id === update.id ? update : item);
 }
