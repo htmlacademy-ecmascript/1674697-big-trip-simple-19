747ca36a097e522a28fbd1d7be15971c1f61730c
a.nemova <carrie@mail.ru>
2023-01-04 12:15:20 +0300

    Изменяет шаблон сортировки

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,10 +1,11 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import { SortType } from '../utils/const.js';
 
-function createSortTemplate() {
+function createSortTemplate(currentSortType) {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <div class="trip-sort__item  trip-sort__item--day">
-        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
+        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" data-sort-type="${SortType.DEFAULT} value="sort-day" ${currentSortType === SortType.DEFAULT ? 'checked' : ''}>
         <label class="trip-sort__btn" for="sort-day">Day</label>
       </div>
 
@@ -19,7 +20,7 @@ function createSortTemplate() {
       </div>
 
       <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" data-sort-type="${SortType.PRICE} value="sort-price" ${currentSortType === SortType.PRICE ? 'checked' : ''}>
         <label class="trip-sort__btn" for="sort-price">Price</label>
       </div>
 
