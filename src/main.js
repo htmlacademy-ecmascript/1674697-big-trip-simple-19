e2eaa623b9710484df3e285374d11e8bdaae5967
a.nemova <carrie@mail.ru>
2023-01-19 11:30:58 +0300

    Рефакторит фильтрацию и добавляет кнопку для новой точки.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,35 @@
-import FilterView from './view/filter-view.js';
-import EventsPresenter from './presenter/events-presenter.js';
 import { render } from './framework/render.js';
+import FilterView from './view/filter-view.js';
+import NewPointButtonView from './view/new-point-button-view';
+import EventsPresenter from './presenter/events-presenter.js';
 import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
-import { generateFilter } from '../src/mock/filter.js';
 
+const siteHeaderElement = document.querySelector('.trip-main');
 const tripFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
+
 const pointsModel = new PointsModel();
 const filterModel = new FilterModel();
-const filters = generateFilter(pointsModel.points);
+const filters = [
+  {
+    type: 'everything',
+    name: 'ALL',
+    count: 0,
+  },
+];
+
 const eventsPresenter = new EventsPresenter({
   eventListContainer: tripEventsElement,
   pointsModel,
 });
 
-render(new FilterView({ filters }), tripFiltersElement);
+render(new FilterView({
+  filters,
+  currentFilterType: 'everything',
+  onFilterTypeChange: () => {}
+}), tripFiltersElement);
+
+render(new NewPointButtonView(), siteHeaderElement);
 
 eventsPresenter.init();
