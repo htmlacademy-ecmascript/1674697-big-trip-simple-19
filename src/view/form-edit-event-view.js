8dedb71bff3bc8db23e78209efb7fabda6fe03a8
a.nemova <carrie@mail.ru>
2023-01-19 11:03:03 +0300

    Реализует удаление.

--- src/view/form-edit-event-view.js
+++ src/view/form-edit-event-view.js
@@ -286,7 +286,7 @@ export default class FormEditEventView extends AbstractStatefulView {
   };
 
   #deleteClickHandler = () => {
-    this.#handleDeleteClick();
+    this.#handleDeleteClick(FormEditEventView.parseStateToPoint(this._state));
   };
 
   removeElement = () => {
