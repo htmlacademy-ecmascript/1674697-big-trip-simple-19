f32e73ce04b88d5edef6b6eb4d5e96b27ac3957d
a.nemova <carrie@mail.ru>
2023-01-19 11:14:22 +0300

    Добавляет модель для фильтра.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import FilterView from './view/filter-view.js';
 import EventsPresenter from './presenter/events-presenter.js';
 import { render } from './framework/render.js';
 import PointsModel from './model/points-model.js';
+import FilterModel from './model/filter-model.js';
 import { generateFilter } from '../src/mock/filter.js';
 
 const tripFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 const pointsModel = new PointsModel();
+const filterModel = new FilterModel();
 const filters = generateFilter(pointsModel.points);
 const eventsPresenter = new EventsPresenter({
   eventListContainer: tripEventsElement,
